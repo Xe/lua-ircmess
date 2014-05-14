408c3ada11351650af6d96523af708f84ced04be
Sam Dodrill <shadow.h511@gmail.com>
2014-05-14 00:00:53 -0700

    tests: do slightly better tests

--- test.lua
+++ test.lua
@@ -2,9 +2,18 @@ require "ircmess"
 
 s = ircmess.parse(":cyka.yolo-swag.com 001 Xena :Welcome to IRC!\r\n")
 
-print(s.source)
-print(s.verb)
-print(#s.argv)
-print(s.argv[1])
-print(s.argv[2])
+assert(s.verb == "001")
+assert(s.source == "cyka.yolo-swag.com")
+assert(#s.argv == 2)
+assert(s.argv[1] == "Xena")
+
+print("Initial tests passed.")
+
+s = ircmess.parse("PING :cyka.yolo-swag.com")
+assert(s.source == "")
+assert(s.verb == "PING")
+assert(#s.argv == 1)
+assert(s.argv[1] == "cyka.yolo-swag.com")
+
+print("All tests passed!")
 
